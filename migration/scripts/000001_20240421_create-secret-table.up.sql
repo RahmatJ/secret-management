805a1efce2336be2f011975b788af8b15161faa2
RahmatJ <rjayanto22.rj@gmail.com>
2024-04-21 16:26:41 +0700

    feat: add migration script to create secret table

---
+++ migration/scripts/000001_20240421_create-secret-table.up.sql
@@ -0,0 +1,11 @@
+create table if not exists secret_management (
+    id varchar(16) primary key unique,
+    user_id varchar(50) not null,
+    api_key varchar(100) not null,
+    expiredDate varchar(15) not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now()
+);
+
+create index if not exists secret_management_user_id_created_at
+    on secret_management (user_id, created_at)
